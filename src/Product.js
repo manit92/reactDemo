c36fabc47cc24f05f5297b2606abb69eece68d35
training_d5.01.02 <training_d5.01.02@accenture.com>
2020-01-23 10:43:41 +0530

    to show product details through id: history

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,16 @@
 
 import React from "react"
+import { withRouter } from "react-router-dom"
 
 
-export default function(props){
-    return ( <div>
+export function Product(props){
+
+    let showProduct = () => {
+        alert(props.product.productid + '....' + props.product.name)
+        localStorage.productid = props.product.productid
+        props.history.push('/details')
+    }
+    return ( <div onClick={showProduct}>
             { props.product  && <div class="card " style={{width: "18rem"}}>
             <img class="card-img-top" src={props.product.image} alt="Card image cap"></img>
             <div class="card-body">
@@ -15,3 +22,5 @@ export default function(props){
         
     )
 }
+
+export default withRouter(Product)
